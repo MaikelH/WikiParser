c6681b09c5e595959ea66cb794c6cfdb54f65175
Maikel H <mh.carducci@gmail.com>
2013-07-27 23:11:48 +0200

    Basic actor setup finished

--- src/main/scala/org/chronos/wikiparsing/workers/PageProcessor.scala
+++ src/main/scala/org/chronos/wikiparsing/workers/PageProcessor.scala
@@ -1,5 +1,9 @@
 package org.chronos.wikiparsing.workers
 
+import akka.actor.Actor
+import akka.event.Logging
+import org.chronos.wikiparsing.messages.Page
+
 /**
  * Created with IntelliJ IDEA.
  * User: Maikel
@@ -7,6 +11,10 @@ package org.chronos.wikiparsing.workers
  * Time: 22:00
  * To change this template use File | Settings | File Templates.
  */
-class PageProcessor {
+class PageProcessor extends Actor {
+  val log = Logging(context.system, this)
 
+  def receive: Actor.Receive = {
+    case Page(content) => log.info("Received page.")
+  }
 }
