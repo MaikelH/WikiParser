c11cad37d04b4eabb2fbe760b7a3f05ab0317002
Maikel H <mh.carducci@gmail.com>
2013-07-29 00:11:08 +0200

    Add basic Task actors

---
+++ src/main/scala/org/chronos/wikiparsing/workers/TotalSentences.scala
@@ -0,0 +1,15 @@
+package org.chronos.wikiparsing.workers
+
+import akka.actor.Actor
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maikel
+ * Date: 29-7-13
+ * Time: 0:04
+ */
+class TotalSentences extends Actor {
+  def receive: Actor.Receive = {
+    case _ =>
+  }
+}
---
+++ src/main/scala/org/chronos/wikiparsing/workers/TotalWords.scala
@@ -0,0 +1,16 @@
+package org.chronos.wikiparsing.workers
+
+import akka.actor.Actor
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maikel
+ * Date: 28-7-13
+ * Time: 21:48
+ * To change this template use File | Settings | File Templates.
+ */
+class TotalWords extends Actor{
+  def receive = {
+    case _ =>
+  }
+}
