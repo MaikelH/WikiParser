aa25e1bf78bdb9cad4a496278f493e448314f5ba
Maikel H <mh.carducci@gmail.com>
2013-07-30 15:02:05 +0200

    Changed Page interface

---
+++ src/test/scala/TotalSentenceTest.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maikel
+ * Date: 30-7-13
+ * Time: 14:02
+ * To change this template use File | Settings | File Templates.
+ */
+class TotalSentenceTest {
+
+}
