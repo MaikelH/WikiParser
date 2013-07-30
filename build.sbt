7fadb5d6afb3d04917b46a14ced5688a2fd44dcf
Maikel H <mh.carducci@gmail.com>
2013-07-30 15:05:21 +0200

    Added test for TotalSentenceCount

--- build.sbt
+++ build.sbt
@@ -10,4 +10,6 @@ libraryDependencies ++= Seq(
 	"com.typesafe.akka" %% "akka-actor" % "2.2.0",
 	"com.typesafe.akka" %% "akka-slf4j"    % "2.2.0",
 	"com.typesafe.akka" %% "akka-remote"   % "2.2.0",
-	"com.typesafe.akka" %% "akka-agent"    % "2.2.0")
+	"com.typesafe.akka" %% "akka-agent"    % "2.2.0",
+	"com.typesafe.akka" %% "akka-testkit"    % "2.2.0",
+	"org.scalatest" % "scalatest_2.10" % "1.9.1" % "test")
--- src/test/scala/TotalSentenceTest.scala
+++ src/test/scala/TotalSentenceTest.scala
@@ -1,3 +1,11 @@
+import org.chronos.wikiparsing.utilities.Page
+import org.chronos.wikiparsing.workers.TotalSentences
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import akka.testkit.TestActorRef
+import akka.actor.ActorSystem
+import org.scalatest.matchers.ShouldMatchers
+
 /**
  * Created with IntelliJ IDEA.
  * User: Maikel
@@ -5,6 +13,33 @@
  * Time: 14:02
  * To change this template use File | Settings | File Templates.
  */
-class TotalSentenceTest {
+class TotalSentenceTest extends FunSuite with BeforeAndAfter with ShouldMatchers{
 
+  var actor: TotalSentences = _
+
+  before {
+    implicit val System = ActorSystem("WikiParser")
+    val actorRef = TestActorRef[TotalSentences]
+    actor = actorRef.underlyingActor
+  }
+
+  test("One sentence") {
+    val page = new Page("Test", " Hallo dit is een test.")
+    actor.processMessage(page) should equal(1)
+  }
+
+  test("Two sentences") {
+    val page = new Page("Test", " Hallo dit is een test. En nog een zin.")
+    actor.processMessage(page) should equal(2)
+  }
+
+  test("Two sentences with abbreviation") {
+    val page = new Page("Test", "Hallo dit is een test. En nog Mr. een zin.")
+    actor.processMessage(page) should equal(2)
+  }
+
+  test("Two sentences with abbreviation at end") {
+    val page = new Page("Test", "Hallo dit is een test. En nog Mr. een zin. PS.")
+    actor.processMessage(page) should equal(3)
+  }
 }
